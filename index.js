3f287b06c83c367f87c5a499f1734d7b8945fe25
andrucuentaofficial <andrucuentaofficial@gmail.com>
2026-04-26 10:46:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const { Client, RemoteAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const { OpenAI } = require('openai');
 
-const openai = new OpenAI({ apiKey: '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' });
+const openai = new OpenAI({ 
+  apiKey: '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' 
+});
 
 const client = new Client({
+    authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-extensions'
+        ],
+        // Esto es lo que le dice a Railway dónde está el Chrome
+        executablePath: '/usr/bin/google-chrome'
     }
 });
 
@@ -17,7 +27,7 @@ client.on('qr', (qr) => {
 });
 
 client.on('ready', () => {
-    console.log('¡BOT ACTIVO!');
+    console.log('¡BOT ACTIVO EN RAILWAY!');
 });
 
 client.on('message', async (msg) => {
